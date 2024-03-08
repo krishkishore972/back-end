b2893ebd1bbb781d534b8eee7e00f0c9c113504a
krishkishore972 <138987031+krishkishore972@users.noreply.github.com>
2024-03-08 12:36:54 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 // require('dotenv').config({path:'./env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
 
-
-
-
-
-
-
-
-
-
-
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !!", err);
+  });
 
 /*
 import express from "express";
